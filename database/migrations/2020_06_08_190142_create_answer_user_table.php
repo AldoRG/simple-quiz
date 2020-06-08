207551b87c82e84b88366a3ec1cbb6bd2b2593f8
Aldo Ruiz <aldo.ruiz@outlook.com>
2020-06-08 13:27:19 -0600

    added models, migration, seeders for Question and Answer

---
+++ database/migrations/2020_06_08_190142_create_answer_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswerUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer_user', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('correct')->default(false);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('question_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer_user');
+    }
+}
---
+++ database/seeds/QuestionSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Question;
+use Illuminate\Database\Seeder;
+
+class QuestionSeeder extends Seeder
+{
+    const WORDS_ANSWER = [
+        'el altavoz' => 'loudspeaker',
+        'el alumno/la alumna' => 'pupil',
+        'el bolígrafo' => 'ballpoint pen',
+        'el borrador' => 'board eraser',
+        'el calendario' => 'calendar',
+        'el cartel/el póster' => 'poster',
+        'el cuaderno' => 'notebook, workbook',
+        'el diccionario' => 'dictionary',
+        'el escritorio' => 'desk',
+        'el estudiante/la estudiante' => 'student'
+    ];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (self::WORDS_ANSWER as $question => $answer) {
+            $q = Question::create([
+                'question' => 'Traducción de: '.$question
+            ]);
+            $a = new \App\Answer(['answer' => $answer]);
+            $q->answer()->save($a);
+        }
+    }
+}
